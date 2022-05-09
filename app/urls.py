6ca989e60f3161592a92ad6c06c7c73293000d8f
Adriano Cerutti <adrianocerutti@gmail.com>
2022-05-09 12:24:25 -0300

    Adicionado código base módulo de pets

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import cliente_views
+from .views import cliente_views, pet_views
 
 urlpatterns = [
     path('cadastrar_cliente', cliente_views.cadastrar_cliente,
@@ -12,4 +12,6 @@ urlpatterns = [
          cliente_views.editar_cliente, name='editar_cliente'),
     path('remover_cliente/<int:id>',
          cliente_views.remover_cliente, name='remover_cliente'),
+    path('cadastrar_pet/<int:id>', pet_views.inserir_pet, name='cadastrar_pet'),
+    path('listar_pet/<int:id>', pet_views.listar_pet_id, name='listar_pet_id'),
 ]
